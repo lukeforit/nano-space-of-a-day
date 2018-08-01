cca9a42215c75b60fd636b69c2b57cbff43c8f2c
ukasz1412 <ukasz1412@gmail.com>
2018-08-01 23:04:18 +0200

    Network - logging Http client

--- app/src/main/java/me/lukeforit/spaceofaday/di/NetModule.java
+++ app/src/main/java/me/lukeforit/spaceofaday/di/NetModule.java
@@ -8,6 +8,8 @@ import me.lukeforit.spaceofaday.BuildConfig;
 import me.lukeforit.spaceofaday.data.source.SpaceRepository;
 import me.lukeforit.spaceofaday.data.source.network.SpaceRepositoryNet;
 import me.lukeforit.spaceofaday.data.source.network.SpaceRestService;
+import okhttp3.OkHttpClient;
+import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.moshi.MoshiConverterFactory;
@@ -20,6 +22,10 @@ public class NetModule {
     public Retrofit provideRetrofit() {
         return new Retrofit.Builder()
                 .baseUrl(BuildConfig.baseUrl)
+                .client(new OkHttpClient.Builder()
+                        .addInterceptor(new HttpLoggingInterceptor()
+                                .setLevel(HttpLoggingInterceptor.Level.BODY))
+                        .build())
                 .addConverterFactory(MoshiConverterFactory.create())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build();
