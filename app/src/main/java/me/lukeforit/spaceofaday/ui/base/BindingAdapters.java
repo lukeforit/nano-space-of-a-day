ec95cbc4c0fb5a6edc754a20b70c18c7c2361e6e
ukasz1412 <ukasz1412@gmail.com>
2018-08-07 22:36:45 +0200

    UI - styling layouts

--- app/src/main/java/me/lukeforit/spaceofaday/ui/base/BindingAdapters.java
+++ app/src/main/java/me/lukeforit/spaceofaday/ui/base/BindingAdapters.java
@@ -1,16 +1,44 @@
 package me.lukeforit.spaceofaday.ui.base;
 
 import android.databinding.BindingAdapter;
+import android.graphics.drawable.Drawable;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.text.TextUtils;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.SimpleTarget;
+import com.bumptech.glide.request.transition.Transition;
 
 public class BindingAdapters {
     @BindingAdapter("image_url")
     public static void bindUrl(ImageView imageView, String url) {
         if (!TextUtils.isEmpty(url)) {
-            Glide.with(imageView.getContext()).load(url).into(imageView);
+            Glide.with(imageView.getContext())
+                    .load(url)
+                    .apply(new RequestOptions().centerCrop())
+                    .into(imageView);
+        }
+    }
+
+    @BindingAdapter("background_url")
+    public static void bindUrl(final ViewGroup viewGroup, String url) {
+        if (!TextUtils.isEmpty(url)) {
+            Glide.with(viewGroup.getContext())
+                    .load(url)
+                    .apply(new RequestOptions().centerCrop())
+                    .into(new SimpleTarget<Drawable>() {
+                        @Override
+                        public void onResourceReady(
+                                @NonNull Drawable resource,
+                                @Nullable Transition<? super Drawable> transition) {
+                            resource.setAlpha(80);
+                            viewGroup.setBackground(resource);
+                        }
+                    });
         }
     }
 }
